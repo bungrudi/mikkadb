d97635dd13b3d5ee008dc66b3eddca6d9c125d8b
rudi <rudi.adianto@ajaib.co.id>
2024-04-28 11:41:15 +0700

    set port and addr from args

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,32 @@ use std::sync::{Arc, Mutex};
 fn main() {
 
     println!("Logs from your program will appear here!");
+    let args: Vec<String> = std::env::args().collect();
+    let mut port = "6379"; // default port
+    let mut addr = "0.0.0.0";
+
+    for i in 0..args.len() {
+        if args[i] == "--port" {
+            if i + 1 < args.len() {
+                port = &args[i + 1];
+            } else {
+                eprintln!("--port argument provided but no port number was given");
+                std::process::exit(1);
+            }
+        }
+        if args[i] == "--addr" {
+            if i + 1 < args.len() {
+                addr = &args[i + 1];
+            } else {
+                eprintln!("--addr argument provided but no address was given");
+                std::process::exit(1);
+            }
+        }
+    }
 
     let redis = Arc::new(Mutex::new(redis::Redis::new()));
 
-    let listener = TcpListener::bind("0.0.0.0:6379").unwrap();
+    let listener = TcpListener::bind(format!("{}:{}",addr,port)).unwrap();
 
     for stream in listener.incoming() {
         match stream {
