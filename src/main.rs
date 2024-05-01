297a50d1a506baf1cddd8745aeeb5daea6f9116b
rudi <rudi.adianto@ajaib.co.id>
2024-05-01 08:23:46 +0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod client_handler;
 mod resp;
 mod redis;
 
+use std::io::{Read, Write};
 use std::net::TcpListener;
 use std::sync::{Arc, Mutex};
 use crate::redis::RedisConfig;
@@ -50,6 +51,46 @@ fn main() {
     let redis = Arc::new(Mutex::new(redis::Redis::new(config.clone())));
 
     let listener = TcpListener::bind(format!("{}:{}",config.addr,config.port)).unwrap();
+
+    // TODO encapsulate startup logic..
+    // as part of startup sequence..
+    // if there is a replicaof host and port we should send PING
+    // to master and wait for PONG.
+    // how do we encapsulate the logic for this?
+
+    // for now let's just code it here.
+    // connect to remote host and port, send PING as RESP.
+    // if we get PONG we are good to go.
+    // if we don't get PONG we should exit.
+    // if we get an error we should exit.
+    // if we get a response that is not PONG we should exit.
+    // if we get a response that is PONG we should continue.
+    if let Some(replicaof_host) = &config.replicaof_host {
+        if let Some(replicaof_port) = &config.replicaof_port {
+            let replicaof_addr = format!("{}:{}", replicaof_host, replicaof_port);
+            match std::net::TcpStream::connect(replicaof_addr) {
+                Ok(mut stream) => {
+                    let ping = "*1\r\n$4\r\nPING\r\n";
+                    stream.write(ping.as_bytes()).unwrap();
+                    let mut buffer = [0; 512];
+                    let bytes_read = stream.read(&mut buffer).unwrap();
+                    let response = std::str::from_utf8(&buffer[0..bytes_read]).unwrap();
+                    if response == "+PONG\r\n" {
+                        println!("replicaof host and port is valid");
+                    } else {
+                        eprintln!("replicaof host and port is invalid");
+                        std::process::exit(1);
+                    }
+                }
+                Err(e) => {
+                    eprintln!("error connecting to replicaof host and port: {}", e);
+                    std::process::exit(1);
+                }
+            }
+        }
+    }
+
+
 
     for stream in listener.incoming() {
         match stream {
